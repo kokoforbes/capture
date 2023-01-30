72e7c685ba83ecb23e787bdbd67e05528987b4b5
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-01-30 17:52:48 +0100

    Implemented react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      {/* <Switch>
+        
+      </Switch> */}
+
+      <Routes>
+        <Route index element={<AboutUs />} />
+        <Route path='work' element={<OurWork />} />
+        <Route path='contact' element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
