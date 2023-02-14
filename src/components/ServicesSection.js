8323891b946cfbab823a3762a9756fc06e7d334b
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-02-14 17:51:16 +0100

    created UseScrol hook

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -6,10 +6,19 @@ import money from "../img/money.svg";
 import home2 from "../img/home2.png";
 import { About, Description, Image } from "../styles";
 import styled from "styled-components";
+import { useScroll } from "./useScroll";
+import { scrollReveal } from "../animation";
 
 const ServicesSection = () => {
+  const [element, controls] = useScroll();
+
   return (
-    <Services>
+    <Services
+      variants={scrollReveal}
+      animate={controls}
+      initial='hidden'
+      ref={element}
+    >
       <Description>
         <h2>
           High <span>quality</span>services
---
+++ src/components/useScroll.js
@@ -0,0 +1,17 @@
+import { useInView } from "react-intersection-observer";
+import { useAnimation } from "framer-motion";
+import { useEffect } from "react";
+
+export const useScroll = () => {
+  const controls = useAnimation();
+
+  const [element, view] = useInView({ threshold: 0.5 });
+
+  if (view) {
+    // controls.start("show");
+  } else {
+    // controls.start("hide");
+  }
+
+  return [element, controls];
+};
